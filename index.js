8b9488e0f91138a32460ed0da863bc07956c45d9
mylimited43 <mylimited43@gmail.com>
2023-02-09 17:39:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// document.querySelector("h1").innerHTML = "Hello World!!!";
+var heading = document.firstElementChild.lastElementChild.firstElementChild
+heading.innerHTML = "Hello Na"
+heading.style.color = "red" 
